b579d697aebad784852f2dc2c63d0289a9923e38
Mariya <m_relaxeena@mail.ru>
2022-11-24 17:38:41 +0400

    дз2

--- Homework1/task2/Program.cs
+++ Homework1/task2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number1 = new int();
+Console.WriteLine("Введите число");
+number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = new int();
+Console.WriteLine("Введите число");
+number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine("Большее число");
+    Console.WriteLine(number1);
+    Console.WriteLine("Меньшее число");
+    Console.WriteLine(number2);
+}
+else
+{
+    Console.WriteLine("Большее число");
+    Console.WriteLine(number2);
+    Console.WriteLine("Меньшее число");
+    Console.WriteLine(number1);
+}
