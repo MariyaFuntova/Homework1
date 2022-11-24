aca0c964f5c7a56355e363586e8d512c4edab898
Mariya <m_relaxeena@mail.ru>
2022-11-24 18:32:45 +0400

    дз8

--- Homework1/task8/Program.cs
+++ Homework1/task8/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 0;
+int n = new int();
+Console.WriteLine("Введите число");
+n = Convert.ToInt32(Console.ReadLine());
+while (a < (n -1))
+{
+    a += 2;
+    Console.WriteLine(a);
+}
+Console.Write("Четные числа от 1 до введеного числа");
+
+
